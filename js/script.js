fd2d722130bd89b17c191d92e91aa67f0675999d
Samar <example@gmail.com>
2022-04-27 10:10:58 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,117 @@
+"use strict"
+
+
+// a = 10;
+// console.log(a);
+// console.log("hello world");
+
+// let number = 3;
+// console.log(number);
+
+// const pi = 3.14;
+
+// const obj = {
+//     a: 10,
+// };
+
+// obj.a = 20;
+// console.log(obj.a);
+
+
+// {
+//     let y = 15;
+// }
+// console.log(y);
+
+
+//2 dars
+// const number = 8;
+// console.log(-4/0);
+
+// console.log('samar'*10) // not a number = son emas 
+
+// const firstName = "9";
+// const bool = "false"
+
+// const isMarried = true;
+
+// console.log(a); // data type null
+
+// let lastName;
+
+// console.log(lastName)
+
+
+// const person = {
+//     firstName: "Anas",
+//     lastName: "Umar",
+//     age: 22,
+//     job: "full stack js dev",
+//     isMarried: false,
+// };
+
+// console.log(person);
+
+// const arr = ['red', 'yellow', 'black'];
+// console.log(arr[0]);
+
+// console.log('hello world');
+
+// alert("hello world")
+
+// const userAge = confirm("siz 12 yoshdamisz");
+// console.log(userAge);
+
+// const userName = prompt('ismiz nima?', "");
+// console.log(userName);
+
+// const answers = [];
+// answers[0] = prompt('ismiz nima?')
+// answers[1] = prompt("familya nima?")
+// answers[2] = prompt('yoshiz nechchi?')
+
+// console.log(answers);
+// document.write(answers)
+
+// const firstName = +prompt('yoshiz', '');
+// console.log(firstName + 10);
+
+// alert('hi world')
+
+// 8-dars
+
+// const firstName = "samar";
+// const age = 19;
+// console.log(`mani ismim ${firstName} yoshim esa ${age}`);
+
+// const xName = prompt('enter your name','');
+// console.log(`user's name ${xName}`);
+
+// const x = 10, 
+//     y = 15;
+// console.log(x + y);
+// console.log(5 - 5);
+// console.log(5 / 5);
+// console.log(5 * 5);
+
+// console.log('hello ' + 'world');
+
+// const num = 4 + 'radio';
+// console.log(typeof num);
+
+// let z = 10;
+// console.log(z++);
+
+// console.log(5%2);
+
+// const q = 15; 
+// const m = 7;
+// console.log(q % m);
+
+// console.log(2 * 3 == '6');
+// console.log(2 * 3 === '6');
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
